82b06e586b1213bf9b5ee5aa8fdf61e3eb48d6c5
Beqa Basiladze <basiladze.beqa@gtu.ge>
2023-06-05 02:36:20 +0400

    discard some changes

---
+++ Library.DAL/Repositories/MenuNavigationRepository.cs
@@ -0,0 +1,31 @@
+﻿using Library.DAL.Interfaces;
+using Library.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DAL.Repositories
+{
+    public class MenuNavigationRepository : IMenuNavigation
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuNavigationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Menu>> GetMenuList()
+        {
+            return await _context.Menus.Include(m => m.SubMenuItems).ToListAsync();
+        }
+
+        public async Task<IEnumerable<SubMenu>> GetSubMenuList()
+        {
+            return await _context.SubMenus.Include(m=>m.MenuItems).ToListAsync();
+        }
+    }
+}
---
+++ Library.Domain/ViewModel/MenuView/MenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Library.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.ViewModel.MenuView
+{
+    public class MenuViewModel
+    {
+        public IEnumerable<Menu> MenuList { get; set; }
+    }
+}
